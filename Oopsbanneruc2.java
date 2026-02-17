9f1816998295ce34e50a040aefc20e718b756415
nimeshjoshii <joshinimesh.91@gmail.com>
2026-02-17 14:50:08 +0530

    Use case 2 added

---
+++ Oopsbanneruc2.java
@@ -0,0 +1,13 @@
+public class Oopsbanneruc2 {
+        public static void main(String[] args){
+        System.out.println("   *       *    **       *** ");
+        System.out.println(" *   *   *   *  *    *   **      ");
+        System.out.println("*     * *     * *     * **       ");
+        System.out.println("*     * *     * *    *   **      ");
+        System.out.println("*     * *     * **       *   ");
+        System.out.println("*     * *     * *              * ");
+        System.out.println("*     * *     * *               *");
+        System.out.println(" *   *   *   *  *              * ");
+        System.out.println("   *       *    *         **   ");
+    }
+}
